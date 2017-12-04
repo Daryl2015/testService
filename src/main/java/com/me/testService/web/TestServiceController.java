25d7571f761541b028112e537390dc581de51b45
jibing <619623172@qq.com>
2017-12-04 16:27:47 +0800

    add json type

--- src/main/java/com/me/testService/web/TestServiceController.java
+++ src/main/java/com/me/testService/web/TestServiceController.java
@@ -1,5 +1,7 @@
 package com.me.testService.web;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,13 +9,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSONObject;
+import com.me.testService.bean.CommonResponse;
+import com.me.testService.bean.TestBean;
+import com.me.testService.util.DataUtils;
+
 @RestController
 @RequestMapping("/test")
+@SuppressWarnings("all")
 public class TestServiceController {
 
     @RequestMapping("/index")
     @ResponseBody
     public String testIndex(HttpServletRequest request, HttpServletResponse response) {
-        return "server is startup.";
+        return "server is startup." + DataUtils.formatDateTime(new Date());
+    }
+
+    @RequestMapping("/json")
+    @ResponseBody
+    public String testJson(HttpServletRequest request, HttpServletResponse response) {
+        TestBean bean = new TestBean();
+        bean.setName("名称");
+        bean.setValue("值");
+        bean.setUpdateTime(new Date());
+        return JSONObject.toJSONString(bean);
+    }
+
+    @RequestMapping("/commJson")
+    @ResponseBody
+    public CommonResponse testCommonResponse(HttpServletRequest request, HttpServletResponse response) {
+        TestBean bean = new TestBean();
+        bean.setName("名称");
+        bean.setValue("123456789");
+        bean.setUpdateTime(new Date());
+        return new CommonResponse(CommonResponse.SUCCESS, CommonResponse.SUCCESS_MSG, bean);
     }
 }
