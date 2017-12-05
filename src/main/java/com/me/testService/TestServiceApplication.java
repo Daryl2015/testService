b5a20243f84026a33e1f2e6866b3df74f389f1a7
jibing <619623172@qq.com>
2017-12-05 15:55:46 +0800

    add eurekaClient

--- src/main/java/com/me/testService/TestServiceApplication.java
+++ src/main/java/com/me/testService/TestServiceApplication.java
@@ -2,14 +2,17 @@ package com.me.testService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 启动
  *
  */
 @SpringBootApplication
-public class TestServiceApplication
-{
+@EnableEurekaClient
+public class TestServiceApplication extends SpringBootServletInitializer {
+
     public static void main( String[] args )
     {
         SpringApplication.run(TestServiceApplication.class, args);
